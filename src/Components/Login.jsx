73ec1284d80a6d6e09d5ff0bb9c63d1c3b5fd753
hectorvlbs <44859441+hectorvlbs@users.noreply.github.com>
2021-03-18 17:52:43 -0700

    input validate

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,22 +1,47 @@
 import React from 'react'
 
 const Login = () => {
+
+    const passLength = 6
+    const [email, setEmail] = React.useState('')
+    const [pass, setPass] = React.useState('')
+
+    const dataProcess = e => {
+        e.preventDefault()
+        if (!email.trim()) {
+            console.log('ingrese email')
+            return
+        } else if (!pass.trim()) {
+            console.log('Ingrese pass')
+            return
+        } else if (pass.length < passLength) {
+            console.log(`pass need ${passLength} chars`)
+            return
+        } else {
+            console.log(`email: ${email}, pass: ${pass}`)
+        }
+    }
+
     return (
         <div className="mt-5">
             <h3 className="text-center">Acceso o registro</h3>
             <hr/>
             <div className="row justify-content-center">
                 <div className="col-12 col-sm-8 col-md-6 col-xl-4">
-                    <form>
+                    <form onSubmit={dataProcess}>
                         <input
                             type="email"
                             className="form-control mb-2"
                             placeholder="Ingrese su email."
+                            onChange={e => setEmail(e.target.value)}
+                            value={email}
                         />
                         <input
                             type="password"
                             className="form-control mb-2"
                             placeholder="Ingrese la contraseña."
+                            onChange={e => setPass(e.target.value)}
+                            value={pass}
                         />
                         <button className="btn btn-dark btn-lg btn-block">
                             Registrarse
